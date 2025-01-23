03e5999cae00ac7957e51fd96fd84de21c1935ad
A-Rehman-arain <160388837+A-Rehman-arain@users.noreply.github.com>
2025-01-23 19:31:13 +0500

    first commit

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -133,7 +133,7 @@ export default function Cart() {
                     className="text-gray-500 hover:text-gray-700"
                     onClick={() => removeFromCart(item.id)}
                   >
-                    <img src='/images/brown-bin.png' className="w-4 h-4" alt="Remove item" />
+                    <Image src='/images/brown-bin.png' className="w-4 h-4" alt="Remove item" />
                   </button>
                 </div>
               ))
